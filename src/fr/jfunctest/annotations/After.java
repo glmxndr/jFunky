dfc633e786eb74721d21f99002a7265bfe7fbe7f
root <root@TyP.(none)>
2010-02-18 20:58:59 +0100

    Adding GNU headers

--- src/fr/jfunctest/annotations/After.java
+++ src/fr/jfunctest/annotations/After.java
@@ -1,8 +1,32 @@
+//    JFuncTest - Functional testig in Java
+//    Copyright (C) 2010 G. Andrieu (subtenante gmail com)
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package fr.jfunctest.annotations;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * 
+ * Use this annotation in a BaseTestCase, to flag the method
+ * that need to be run after all the {@link Test} methods have been run.
+ * 
+ * @author G.Andrieu subtenante@gmail.com
+ *
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface After {
 
